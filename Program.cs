05a78f912888fac78e2421c382c4e0141c6949b7
Admin <Admin@DESKTOP-89NTJEN>
2019-05-27 08:50:32 +1200

    code

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace starPrint
 
             int i, j, rows;
             Console.Write("Input number of rows : ");
-            rows = Convert.ToInt32(Console.ReadLine());
+            rows = int.Parse(Console.ReadLine());
             for (i = 1; i <= rows; i++)
             {
                 for (j = 1; j <= i; j++)
